aa275b5b0e5d7e958f94d11173c2cceb912a9f85
Valentina Tabares <valentinatabares212@gmail.com>
2026-01-08 18:59:55 -0500

    feat: add contact API endpoint

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
+import contactRoutes from "./routes/contact.routes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/contact", contactRoutes);
 
 app.get("/", (req, res) => {
   res.send("API running ");
